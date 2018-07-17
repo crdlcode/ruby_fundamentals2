80abfe3a94819e1840103394bbc85be5e06beb18
curtischatch <curtis.chat.hk@gmail.com>
2018-07-17 15:49:15 -0400

    started e8

---
+++ exercise8.rb
@@ -0,0 +1,20 @@
+# methods for repeating entries
+def how_far (num)
+  puts "how far did you #{num} run?"
+end
+
+def how_long (num,distance)
+  puts "How long did person #{num} run take to run #{distance} metres?"
+
+#methods repeating calculations
+def to_seconds(distance,time)
+
+
+end
+
+
+end
+
+speed1
+speed2
+speed3
