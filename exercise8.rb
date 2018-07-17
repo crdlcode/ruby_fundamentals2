a8e60f40859dd3ed43f8e8b7df7cac9efa1bb496
curtischatch <curtis.chat.hk@gmail.com>
2018-07-17 16:12:30 -0400

    scaled

--- exercise8.rb
+++ exercise8.rb
@@ -1,10 +1,11 @@
-# methods for repeating entries
-def how_far (num)
+# methods primary repeating entries
+def ask_distance (num)
   puts "how far did you #{num} run?"
 end
 
-def how_long (num,distance)
+def ask_time (num,distance)
   puts "How long did person #{num} run take to run #{distance} metres?"
+end
 
 #methods repeating calculations
 def to_seconds(mins)
@@ -13,11 +14,30 @@ end
 def calc_speed(distance,time)
   (distance / time.to_f).round(2)
 end
-#methods for repeating runners
-
-
+#methods repeating runners
+def runner(num)
+  ask_distance(num)
+  distance1 = gets.to_f
+  ask_time(num,distance1)
+  mins1 = gets.to_f
+  speed1 = calc_speed(distance1,to_seconds(mins1))
 end
 
-speed1
-speed2
-speed3
+#scaling multiple runners
+speed1 = runner(1)
+speed2 = runner(2)
+speed3 = runner(3)
+
+#speed comparison
+# system not yet scaled in speed comparison
+if speed3 > speed2 && speed3 > speed1
+  puts "Person 3 was the fastest at #{speed3} m/s"
+elsif speed2 > speed3 && speed2 > speed1
+  puts "Person 2 was the fastest at #{speed2} m/s"
+elsif speed1 > speed3 && speed1 > speed2
+  puts "Person 1 was the fastest at #{speed1} m/s"
+elsif speed1 == speed2 && speed2 == speed3
+  puts "Everyone tied at #{speed1} m/s"
+else
+  puts "Well done everyone!"
+end
